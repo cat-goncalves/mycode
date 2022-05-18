f45b2d206658d280c3a0097d0c2062257f2c618c
Catarina Goncalves <cat-goncalves@users.noreply.github.com>
2022-05-18 15:49:45 +0000

    Day 3 warmup

---
+++ warmup/day-3.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+Deadpool = {
+    "Real name": "Wade Winston Wilson",
+    "Team Affiliations": [
+        "Thunderbolts",
+        "X-Force",
+        "Agency X",
+        "Deadpool Corps.",
+        "Great Lakes Initiative",
+        "Weapon X",
+        "Landau",
+        "Luckman, and Lake",
+        "Maggia",
+        "Frightful Four",
+        "Secret Defenders",
+        "Heroes for Hire",
+        "Six Pack",
+        "X-Men",
+    ],
+    "Aliases": [
+        "Merc with a Mouth",
+        "Jack",
+        "Wade T. Wilson",
+        "Mithras",
+        "Johnny Silvini",
+        "Thom Cruz (when confused for Tom Cruise after a magic spell restores his face)",
+        "Hulkpool (after turned into a hulk)",
+        "Wildcard",
+    ],
+    "Base of Operations": "Nomadic",
+    "Powers": [
+        "Accelerated Healing Factor",
+        "Extended longevity",
+        "Immunity to telepathy",
+        "teleportation (aided by machine)",
+        "holographic disguise (aided by machine)",
+        "Superhuman stamina",
+        "agility",
+        "flexibility and reflexes",
+        "may or may not be aware of the world beyond the fourth wall (IE, might know he is fictional)",
+    ],
+}
+
+print(f"Deadpool, aka {Deadpool['Real name']}, Team affiliations: {Deadpool['Team Affiliations'][-1]}, aliases: {Deadpool['Aliases'][5]}, Base of operations: {Deadpool['Base of Operations']}, Powers: {Deadpool['Powers'][2]}")
